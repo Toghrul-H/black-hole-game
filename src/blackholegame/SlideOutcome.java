e31cc6ec634a8eec0ac4ba849b65e1313de6ca53
Toghrul Hasanli <toghrulhasanli2@gmail.com>
2025-11-18 19:28:03 +0100

    Add files via upload

---
+++ src/blackholegame/SlideOutcome.java
@@ -0,0 +1,12 @@
+package blackholegame;
+
+public class SlideOutcome {
+    
+    public final Pos dest;
+    public final boolean fallsIn;
+    
+    public SlideOutcome(Pos dest, boolean fallsIn){
+        this.dest=dest; 
+        this.fallsIn=fallsIn; 
+    }
+}
